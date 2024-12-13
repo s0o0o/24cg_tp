9a27bdae1b6467bc08fbaa3437a36737be8c3f4b
yeonsu <yeonsu9205@naver.com>
2024-12-13 15:52:07 +0900

    Update PlayerObject.cpp

--- FinalProject/MyGame/PlayerObject.cpp
+++ FinalProject/MyGame/PlayerObject.cpp
@@ -29,51 +29,81 @@ void PlayerObject::initilize()
 
 }
 
+float playerLimit = 2.f;
 
 void PlayerObject::update(float elapseTime)
 {
 
 	glm::vec3 dir(0.f);
 	if (isWPressed) {
-		if (worldTransform[3][2] >= -20.f and worldTransform[3][2] <= 20.f
-			and worldTransform[3][0] >= -40.f and worldTransform[3][0] <= 40.f) {
-			dir += getLook();
+		
+		glm::vec3 newPosition = worldTransform[3]; // 현재 위치를 복사
+		newPosition += getLook(); // 이동 방향 추가
+
+		// X축과 Z축 범위를 체크
+		if (newPosition.z > -20.f + playerLimit && newPosition.z < 20.f - playerLimit &&
+			newPosition.x > -40.f + playerLimit && newPosition.x < 40.f - playerLimit) {
+			dir += getLook(); // 범위를 초과하지 않으면 이동
 		}
 		else {
-		
+			if (newPosition.z <= -20.f + playerLimit) dir.z = (-20.f + playerLimit) - worldTransform[3][2];
+			if (newPosition.z >= 20.f - playerLimit) dir.z = (20.f - playerLimit) - worldTransform[3][2];
+			if (newPosition.x <= -40.f + playerLimit) dir.x = (-40.f + playerLimit) - worldTransform[3][0];
+			if (newPosition.x >= 40.f - playerLimit) dir.x = (40.f - playerLimit) - worldTransform[3][0];
 		}
-
 	}
 	if (isAPressed) {
-		if (worldTransform[3][2] >= -20.f and worldTransform[3][2] <= 20.f
-			and worldTransform[3][0] >= -40.f and worldTransform[3][0] <= 40.f) {
-			dir += getRight();
+		glm::vec3 newPosition = worldTransform[3]; // 현재 위치를 복사
+		newPosition += getRight(); // 이동 방향 추가
+
+		// X축과 Z축 범위를 체크
+		if (newPosition.z > -20.f + playerLimit && newPosition.z < 20.f - playerLimit &&
+			newPosition.x > -40.f + playerLimit && newPosition.x < 40.f - playerLimit) {
+			dir += getRight(); // 범위를 초과하지 않으면 이동
 		}
 		else {
-		
+			if (newPosition.z <= -20.f + playerLimit) dir.z = (-20.f + playerLimit) - worldTransform[3][2];
+			if (newPosition.z >= 20.f - playerLimit) dir.z = (20.f - playerLimit) - worldTransform[3][2];
+			if (newPosition.x <= -40.f + playerLimit) dir.x = (-40.f + playerLimit) - worldTransform[3][0];
+			if (newPosition.x >= 40.f - playerLimit) dir.x = (40.f - playerLimit) - worldTransform[3][0];
 		}
 	}
 
 	if (isSPressed) {
-		if (worldTransform[3][2] >= -20.f and worldTransform[3][2] <= 20.f
-			and worldTransform[3][0] >= -40.f and worldTransform[3][0] <= 40.f) {
-			dir -= getLook();
+		glm::vec3 newPosition = worldTransform[3]; // 현재 위치를 복사
+		newPosition -= getLook(); // 이동 방향 추가
+
+		// X축과 Z축 범위를 체크
+		if (newPosition.z > -20.f + playerLimit && newPosition.z < 20.f - playerLimit &&
+			newPosition.x > -40.f + playerLimit && newPosition.x < 40.f - playerLimit) {
+			dir -= getLook(); // 범위를 초과하지 않으면 이동
 		}
 		else {
-			
+			if (newPosition.z <= -20.f + playerLimit) dir.z = (-20.f + playerLimit) - worldTransform[3][2];
+			if (newPosition.z >= 20.f - playerLimit) dir.z = (20.f - playerLimit) - worldTransform[3][2];
+			if (newPosition.x <= -40.f + playerLimit) dir.x = (-40.f + playerLimit) - worldTransform[3][0];
+			if (newPosition.x >= 40.f - playerLimit) dir.x = (40.f - playerLimit) - worldTransform[3][0];
 		}
 
 	}
 
 	if (isDPressed) {
-		if (worldTransform[3][2] >= -20.f and worldTransform[3][2] <= 20.f
-			and worldTransform[3][0] >= -40.f and worldTransform[3][0] <= 40.f) {
-			dir -= getRight();
+		glm::vec3 newPosition = worldTransform[3]; // 현재 위치를 복사
+		newPosition -= getRight(); // 이동 방향 추가
+
+		// X축과 Z축 범위를 체크
+		if (newPosition.z > -20.f + playerLimit && newPosition.z < 20.f - playerLimit &&
+			newPosition.x > -40.f + playerLimit && newPosition.x < 40.f - playerLimit) {
+			dir -= getRight(); // 범위를 초과하지 않으면 이동
 		}
 		else {
-		
-
+			if (newPosition.z <= -20.f + playerLimit) dir.z = (-20.f + playerLimit) - worldTransform[3][2];
+			if (newPosition.z >= 20.f - playerLimit) dir.z = (20.f - playerLimit) - worldTransform[3][2];
+			if (newPosition.x <= -40.f + playerLimit) dir.x = (-40.f + playerLimit) - worldTransform[3][0];
+			if (newPosition.x >= 40.f - playerLimit) dir.x = (40.f - playerLimit) - worldTransform[3][0];
 		}
+
+		
 	}
 
 
